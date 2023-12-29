5f1786b71becbdaf946e4acc6d875ef0e81d195d
ravithakur24 <ravithakur9824@gmail.com>
2023-12-29 20:39:13 +0530

    29-dec-2023 for loop practice

--- practices.js
+++ practices.js
@@ -1,7 +1,34 @@
-// switch(condtion){
+// switch(day.getday){
 //     case 1: monday;
 //     break;
 //     case 2: tuesday;
 //     break;
     
 // }
+
+
+// for loop  incremental
+
+function forloopexample(){
+
+    let x=100;
+    // let day =["monday","tuesday","wednesday","thrusday","friday","saturday"]
+   
+    for (let x = 0; x<100; x++) {
+       console.log(x)
+
+            
+    }
+}
+
+function printvalue(){
+
+    
+    // let day =["monday","tuesday","wednesday","thrusday","friday","saturday"]
+   
+    for (let y =100; y>=1; --y) {
+       console.log(y)
+
+            
+    }
+}
