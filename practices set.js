f7788f0c2fa6e20f304b31e270274c8e35cc3d96
ravithakur24 <ravithakur9824@gmail.com>
2024-01-02 18:48:59 +0530

    date 02-jan -2023 commit

---
+++ practices set.js
@@ -0,0 +1,30 @@
+//operater 
+ 
+
+// let a=5;
+// let b=2;
+// console.log("a+b",a+b)
+// console.log("a-b",a-b)
+// console.log("++a",++a)
+// console.log("a++",a++)
+
+// console.log("--b",--b)
+// console.log("b--",b--)
+
+
+// syntax condition 
+
+// let a=3;
+// let b=3;
+
+// if (a===b){
+//     alert("value are true")
+
+// }
+// else if (a==b){
+//     alert("else if part executed")
+// }
+
+// else{
+//     alert("else part executed")
+// }
